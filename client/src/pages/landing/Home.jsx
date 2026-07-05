f4b11b196e126cdc754ffb9d611e72343f2e83bc
Aditya Saini <adityasaini35947@gmail.com>
2026-07-05 22:19:45 +0530

    Add application pages

---
+++ client/src/pages/landing/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/landing/Navbar/Navbar";
+import Hero from "../../components/landing/Hero/Hero";
+import Statistics from "../../components/landing/Statistics/Statistics";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Statistics />
+    </>
+  );
+};
+
+export default Home;
