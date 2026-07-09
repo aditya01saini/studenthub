b51e89a8d3e745f24e7780faa67d4267c549c793
Aditya Saini <adityasaini35947@gmail.com>
2026-07-09 12:04:23 +0530

    Update server configuration

--- server/src/server.js
+++ server/src/server.js
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-    console.log("server is runnong on PORT", {PORT})
+    console.log(`Server is running on PORT ${PORT}`)
 })
