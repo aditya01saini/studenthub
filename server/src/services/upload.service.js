02f2c34581f25873236aa1a4897eebce897052bc
Aditya Saini <adityasaini35947@gmail.com>
2026-07-10 14:15:39 +0530

    Add upload service

---
+++ server/src/services/upload.service.js
@@ -0,0 +1,25 @@
+import cloudinary from "../config/cloudinary.js";
+import fs from "fs";
+
+export const uploadToCloudinary = async (
+  filePath,
+  folder,
+  resourceType = "image"
+) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder,
+      resource_type: resourceType,
+    });
+
+    fs.unlinkSync(filePath);
+
+    return result.secure_url;
+  } catch (error) {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
+    throw error;
+  }
+};
