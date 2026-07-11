a21e9c11aabeb69fbd09c9f39bdc6475a9a57856
Aditya Saini <adityasaini35947@gmail.com>
2026-07-11 16:57:29 +0530

    Update app configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import errorHandler from "./middlewares/error.middleware.js";
+import internshipRoutes from "./routes/internship.routes.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import studentRoutes from "./routes/student.routes.js";
+
+import recruiterRoutes from "./routes/recruiter.routes.js";
 const app = express();
 
 app.use(cors());
@@ -20,6 +23,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/student", studentRoutes);
+app.use("/api/v1/recruiter", recruiterRoutes);
+app.use("/api/v1/internships", internshipRoutes);
 app.use(errorHandler);
 
 export default app;
