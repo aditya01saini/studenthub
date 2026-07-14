b85dfd7ead7b8373ba01ce337a1b6dbe34894bc7
Aditya Saini <adityasaini35947@gmail.com>
2026-07-14 17:04:54 +0530

    Register application routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import errorHandler from "./middlewares/error.middleware.js";
 import internshipRoutes from "./routes/internship.routes.js";
+import applicationRoutes from "./routes/application.routes.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import studentRoutes from "./routes/student.routes.js";
@@ -25,6 +26,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/student", studentRoutes);
 app.use("/api/v1/recruiter", recruiterRoutes);
 app.use("/api/v1/internships", internshipRoutes);
+app.use("/api/v1/applications", applicationRoutes);
 app.use(errorHandler);
 
 export default app;
